7035524e5a887808eafb1dfeb23749e5f41c20f3
Julian David <anlijudavid@hotmail.com>
2016-09-08 15:12:05 -0500

    notificaciones

--- client/containers/DashBoard/Childrens/Rol/Insertar.container.jsx
+++ client/containers/DashBoard/Childrens/Rol/Insertar.container.jsx
@@ -9,58 +9,82 @@ import {connect} from 'react-redux';
 import InsertarComponent from '../../../../components/Dashboard/Childrens/Rol/Insertar.component.jsx';
 import * as RolActions from '../../../../actions/rol.js';
 
+import * as Notification from '../../../../components/MiniComponents/Notifications.js';
+
+import NotificationSystem from 'react-notification-system';
+
 class InsertarRol extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			descripcion: ''
-		};
+    this.state = {
+      descripcion: ''
+    };
 
-		this._handleClick = this._handleClick.bind(this);
-		this._onChangeInput = this._onChangeInput.bind(this);
-	}
+    this._handleClick = this._handleClick.bind(this);
+    this._onChangeInput = this._onChangeInput.bind(this);
+  }
 
-	_handleClick() {
-		console.log('Rgistrando');
+  componentDidMount() {
+    this._notificationSystem = this.refs.notificationSystem;
+  }
 
-		const {actions} = this.props;
+  _handleClick() {
+    console.log('Rgistrando');
 
-		actions.registrar({descripcion: this.state.descripcion}).then(() => {
-			console.log('insert rol ===>>=>==> ', this.props.rol);
-		}).catch(err => {
-			console.log('>>=>==> ', err);
-		});
-	}
+    const {actions} = this.props;
 
-	_onChangeInput(event) {
-		this.setState({descripcion: event.target.value});
-	}
+    actions.registrar({ descripcion: this.state.descripcion }).then(() => {
+      console.log('insert rol ===>>=>==> ', this.props.rol);
 
-	render() {
-		const {rol} = this.props;
-		var data = 'data init';
+      // Notification.create('Hola!', 'Hola este es el cuerpo!');
 
-		return (
-			<form>
-				<InsertarComponent onChangeText={this._onChangeInput}/>
+      this._notificationSystem.addNotification({
+        message: 'Registrado exitosamente!',
+        level: 'success',
+        autoDismiss: 5
+      });
+    }).catch(err => {
+      console.log('>>=>==> ', err);
 
-				<button type="button" className="btn btn-primary" onClick={this._handleClick}>Insertar</button>
-			</form>
-		);
-	}
+      this._notificationSystem.addNotification({
+        message: 'Error al registrar!',
+        level: 'danger',
+        autoDismiss: 5
+      });
+    });
+  }
+
+  _onChangeInput(event) {
+    this.setState({ descripcion: event.target.value });
+  }
+
+  render() {
+    const {rol} = this.props;
+    var data = 'data init';
+
+    return (
+      <form>
+        <InsertarComponent onChangeText={this._onChangeInput}/>
+
+        <button type="button" className="btn btn-primary" onClick={this._handleClick}>Insertar</button>
+
+        <NotificationSystem ref="notificationSystem" />
+      </form>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	// console.log("Los state de rol:", state);
+  // console.log("Los state de rol:", state);
 
-	return {rol: state.rol};
+  return { rol: state.rol };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(RolActions, dispatch)
-	};
+  return {
+    actions: bindActionCreators(RolActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(InsertarRol);
