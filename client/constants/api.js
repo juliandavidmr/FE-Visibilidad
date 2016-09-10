7d8f3ea181781db39aaf20fae9a238c7ab728ed2
Julian David <anlijudavid@hotmail.com>
2016-09-09 22:00:18 -0500

    constants preparados

--- client/constants/api.js
+++ client/constants/api.js
@@ -5,9 +5,26 @@ var URL = 'http://172.16.31.150/semilleros/api';
  */
 export const ROL_GET_ROLES = URL + '/rol/get_rol';
 export const ROL_POST_ROL = URL + '/rol/insert_rol';
+export const ROL_DELETE_ROL = URL + '/rol/delete_rol';
 
 /**
  * Semilleros
  */
-export const SEMILLERO_GET_SEMILLEROS = '/semillero/get_semillero';
-export const SEMILLERO_POST_SEMILLERO = '/semillero/insert_semillero';
+export const SEMILLERO_GET_SEMILLEROS = URL + '/semillero/get_semillero';
+export const SEMILLERO_POST_SEMILLERO = URL + '/semillero/insert_semillero';
+export const SEMILLERO_DELETE_SEMILLERO = URL + '/semillero/delete_semillero';
+
+/**
+ * Actividades
+ */
+export const ACTIVIDAD_GET_ACTIVIDADES = URL + '/actividad/get_actividad';
+export const ACTIVIDAD_POST_ACTIVIDAD = URL + '/actividad/insert_actividad';
+export const ACTIVIDAD_DELETE_ACTIVIDAD = URL + '/actividad/delete_actividad';
+
+/**
+ * Adjunto tareas
+ */
+export const ACTIVIDAD_GET_ADJUNTO_TAREAS = URL + '/actividad/get_adjunto_tareas';
+export const ACTIVIDAD_POST_ADJUNTO_TAREAS = URL + '/actividad/insert_adjunto_tareas';
+export const ACTIVIDAD_DELETE_ADJUNTO_TAREAS = URL + '/actividad/delete_adjunto_tareas';
+
