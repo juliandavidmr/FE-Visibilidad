65e955c69c9608945757f6ec450274ba7f4d98d5
Julian David <anlijudavid@hotmail.com>
2016-09-20 09:54:24 -0500

    component de cursos

--- client/components/Index/index.jsx
+++ client/components/Index/index.jsx
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 
-import HeaderComponent from './Header/Header.component.jsx';
+import CursosComponent from './Cursos/Cursos.component.jsx';
 import MenuComponent from './Menu/Menu.component.jsx';
 import SliderComponent from './Slider/Slider.component.jsx';
 import ServiceComponent from './Service/Service.component.jsx';
@@ -91,156 +91,39 @@ class IndexComponent extends Component {
           }
         ]}/>
 
-        <section id="mu-latest-courses">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 col-md-12">
-                <div className="mu-latest-courses-area">
-                  <div className="mu-title">
-                    <h2>Latest Courses</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Distinctio ipsa ea maxime mollitia, vitae voluptates, quod at, saepe reprehenderit totam aliquam architecto fugiat sunt animi!</p>
-                  </div>
-                  <div id="mu-latest-course-slide" className="mu-latest-courses-content">
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/1.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Drawing</a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>90Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/2.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Engineering
-                            </a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>75Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/3.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Academic</a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>45Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/1.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Drawing</a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>90Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/2.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Engineering
-                            </a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>75Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-xs-12">
-                      <div className="mu-latest-course-single">
-                        <figure className="mu-latest-course-img">
-                          <a href="#"><img src="assets/assets/img/courses/3.jpg" alt="img"/></a>
-                          <figcaption className="mu-latest-course-imgcaption">
-                            <a href="#">Academic</a>
-                            <span>
-                              <i className="fa fa-clock-o"></i>45Days</span>
-                          </figcaption>
-                        </figure>
-                        <div className="mu-latest-course-single-content">
-                          <h4>
-                            <a href="#">Lorem ipsum dolor sit amet.</a>
-                          </h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi quisquam modi dolore, dicta obcaecati architecto quidem ullam quia.</p>
-                          <div className="mu-latest-course-single-contbottom">
-                            <a className="mu-course-details" href="#">Details</a>
-                            <span className="mu-course-price" href="#">$165.00</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <CursosComponent 
+          title="Latest Courses" 
+          description="Lorem saepe reprehenderit totam aliquam architecto fugiat sunt animi!"
+          items={[{
+            img: 'assets/assets/img/courses/1.jpg',
+            title: 'Un curso 1',
+            time: '90Days',
+            description: 'Una descripcion corta 1',
+            resume: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi.',
+            price: '$165.00'
+          }, {
+            img: 'assets/assets/img/courses/1.jpg',
+            title: 'Un curso 1',
+            time: '90Days',
+            description: 'Una descripcion corta 1',
+            resume: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi.',
+            price: '$165.00'
+          }, {
+            img: 'assets/assets/img/courses/1.jpg',
+            title: 'Un curso 1',
+            time: '90Days',
+            description: 'Una descripcion corta 1',
+            resume: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi.',
+            price: '$165.00'
+          }, {
+            img: 'assets/assets/img/courses/1.jpg',
+            title: 'Un curso 1',
+            time: '90Days',
+            description: 'Una descripcion corta 1',
+            resume: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Amet quod nisi.',
+            price: '$165.00'
+          }]} 
+        />
 
         <SemillerosComponent 
           title="Semilleros de investigación" 
